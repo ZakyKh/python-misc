731fcc378cdc240d2f9c6b6fb597cedad212a7a8
ZakyKh <mzakykhairuddin@gmail.com>
2023-08-01 21:53:35 +0700

    add more example list comprehension with conditional

--- list_comprehension.py
+++ list_comprehension.py
@@ -14,3 +14,12 @@ z = {
 	}
 }
 print([ "{}={}".format(k, v) for k, v in z.items() ])
+
+n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+print([ v for v in n if v % 2 == 0 ])
+print([ f"{v} Genap" if v % 2 == 0 else f"{v} Ganjil" for v in n ])
+
+m = [1, -2, 3, 4, -5, 6, -7, -8, 9, 10, -11, 12, 13, -14, -15]
+print([ f"{v} Genap" if v % 2 == 0 else f"{v} Ganjil" for v in m if v >= 0 ])
+print([ f"{v} Genap" if v % 2 == 0 else f"{v} Ganjil" for v in m if v >= 0 if v % 2 == 0 ])
+print([ f"{v} Genap" if v % 2 == 0 else f"{v} Ganjil" for v in m if v >= 0 and v % 2 == 1 ])
