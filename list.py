b4fefc4ecfe982ed3e8162ab51b7629685827d05
ZakyKh <mzakykhairuddin@gmail.com>
2023-08-01 21:33:07 +0700

    add more example of list

--- list.py
+++ list.py
@@ -7,6 +7,10 @@ print("arr:", arr)
 print("arr[0]:", arr[0])
 print("arr[5]:", arr[5])
 
+# Print array element with index counting from behind
+print("arr[-1]:", arr[-1])
+print("arr[-8]:", arr[-8])
+
 # Print array slice from start index (inclusive) to end index (exclusive)
 print("arr[0:5]:", arr[0:5])
 print("arr[2:8]:", arr[2:8])
@@ -41,3 +45,16 @@ print("arr[::-2]:", arr[::-2])
 
 # Print array from beginning to end, with (X=1) 'jump' (index difference)
 print("arr[::]:", arr[::])
+
+# Empty lists are generated from invalid beginning-end indexing
+print("arr[-1:-9]:", arr[-1:-9])
+print("arr[8:0]:", arr[8:0])
+print("arr[9123:-22]:", arr[9123:-22])
+
+# Underlimit start-index & overlimit end-index are accepted
+print("arr[-12:]:", arr[-12:])
+print("arr[:999]:", arr[:999])
+
+# Invalid index cause error
+# print("arr[-999]:", arr[-999])
+# print("arr[999]:", arr[999])
